10bbc7d8c113827e2af2781a09719086c28ea0c6
austincunningham <austincunningham@oceanfree.net>
2016-09-21 12:26:12 +0100

    setup listener for geolocation text change

--- app/src/main/java/org/wit/myrent/MyRentActivity.java
+++ app/src/main/java/org/wit/myrent/MyRentActivity.java
@@ -2,15 +2,25 @@ package org.wit.myrent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.text.Editable;
 
-public class MyRentActivity extends AppCompatActivity {
+public class MyRentActivity extends AppCompatActivity implements TextWatcher {
+
+    private EditText geolocation;
+    private Residence residence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myrent);
+
+        geolocation = (EditText) findViewById(R.id.geolocation);
+        residence = new Residence();
+        geolocation.addTextChangedListener(this);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -25,5 +35,20 @@ public class MyRentActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        residence.setGeolocation(editable.toString());
     }
 }
