7f9f1e76ff294d537a6c83f241a556cee81274a2
austincunningham <austincunningham@oceanfree.net>
2016-09-21 11:18:17 +0100

    menu now working

--- app/src/main/java/org/wit/myrent/MyRentActivity.java
+++ app/src/main/java/org/wit/myrent/MyRentActivity.java
@@ -2,6 +2,8 @@ package org.wit.myrent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MyRentActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MyRentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myrent);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.myrent, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
