a28a15f2b38ad0afa71893d627f969222b10228e
austincunningham <austincunningham@oceanfree.net>
2016-09-23 13:09:30 +0100

    added ResidenceListActivity

--- app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
@@ -1,7 +1,29 @@
 package org.wit.myrent.activities;
 
-/**
- * Created by ictskills on 23/09/16.
- */
-public class ResidenceListActivity {
+import org.wit.myrent.R;
+import org.wit.myrent.app.MyRentApp;
+import org.wit.myrent.models.Portfolio;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import android.os.Bundle;
+
+public class ResidenceListActivity extends AppCompatActivity
+{
+    private ListView listView;
+    private Portfolio portfolio;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.app_name);
+        setContentView(R.layout.activity_residencelist);
+
+        listView = (ListView) findViewById(R.id.residenceList);
+
+        MyRentApp app = (MyRentApp) getApplication();
+        portfolio = app.portfolio;
+    }
 }
+
