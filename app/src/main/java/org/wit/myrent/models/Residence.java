25c7ac4f641fa9644e9cd5c015609f629a49732f
austincunningham <austincunningham@oceanfree.net>
2016-09-26 11:56:30 +0100

    Added jsonObject to residence model for model backup

--- app/src/main/java/org/wit/myrent/models/Residence.java
+++ app/src/main/java/org/wit/myrent/models/Residence.java
@@ -2,11 +2,17 @@ package org.wit.myrent.models;
 
 import java.util.Date;
 import java.util.Random;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Residence
 {
     public Long id;
     public Long date;
+    private static final String JSON_ID             = "id"            ;
+    private static final String JSON_GEOLOCATION    = "geolocation"   ;
+    private static final String JSON_DATE           = "date"          ;
+    private static final String JSON_RENTED         = "rented"        ;
 
     //a latitude longitude pair
     //example "52.4566,-6.5444"
@@ -20,6 +26,24 @@ public class Residence
         geolocation = "52.253456,-7.187162";
     }
 
+    public Residence(JSONObject json) throws JSONException
+    {
+        id            = json.getLong(JSON_ID);
+        geolocation   = json.getString(JSON_GEOLOCATION);
+        date          = json.getLong(JSON_DATE);
+        rented        = json.getBoolean(JSON_RENTED);
+    }
+
+    public JSONObject toJSON() throws JSONException
+    {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID            , Long.toString(id));
+        json.put(JSON_GEOLOCATION   , geolocation);
+        json.put(JSON_DATE          , date);
+        json.put(JSON_RENTED        , rented);
+        return json;
+    }
+    
     public void setGeolocation(String geolocation)
     {
         this.geolocation = geolocation;
