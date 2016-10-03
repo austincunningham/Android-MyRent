1fb1624585fdd3f058370ba4b0d428c5c3e80432
austincunningham <austincunningham@oceanfree.net>
2016-10-03 10:21:30 +0100

    update residence model to include tenant

--- app/src/main/java/org/wit/myrent/models/Residence.java
+++ app/src/main/java/org/wit/myrent/models/Residence.java
@@ -5,6 +5,9 @@ import java.util.Random;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Context;
+import org.wit.myrent.R;
+
 public class Residence
 {
     public Long id;
@@ -13,6 +16,8 @@ public class Residence
     private static final String JSON_GEOLOCATION    = "geolocation"   ;
     private static final String JSON_DATE           = "date"          ;
     private static final String JSON_RENTED         = "rented"        ;
+    private static final String JSON_TENANT         = "tenant"        ;
+    public String tenant;
 
     //a latitude longitude pair
     //example "52.4566,-6.5444"
@@ -24,6 +29,7 @@ public class Residence
         id = new Random().nextLong();
         date = new Date().getTime();
         geolocation = "52.253456,-7.187162";
+        tenant = ":none presently";
     }
 
     public Residence(JSONObject json) throws JSONException
@@ -32,6 +38,7 @@ public class Residence
         geolocation   = json.getString(JSON_GEOLOCATION);
         date          = json.getLong(JSON_DATE);
         rented        = json.getBoolean(JSON_RENTED);
+        tenant        = json.getString(JSON_TENANT);
     }
 
     public JSONObject toJSON() throws JSONException
@@ -41,6 +48,7 @@ public class Residence
         json.put(JSON_GEOLOCATION   , geolocation);
         json.put(JSON_DATE          , date);
         json.put(JSON_RENTED        , rented);
+        json.put(JSON_TENANT        , tenant);
         return json;
     }
 
@@ -61,6 +69,27 @@ public class Residence
     private String dateString() {
         String dateFormat = "EEE d MMM yyyy H:mm";
         return android.text.format.DateFormat.format(dateFormat, date).toString();
+    }
+
+    public String getResidenceReport(Context context) {
+        String rentedString = "";
+        if (rented) {
+            rentedString = context.getString(R.string.residence_report_rented);
+        }
+        else {
+            rentedString = context.getString(R.string.residence_report_not_rented);
+        }
+
+        String prospectiveTenant = tenant;
+        if (tenant == null) {
+            prospectiveTenant = context.getString(R.string.residence_report_nobody_interested);
+        }
+        else {
+            prospectiveTenant = context.getString(R.string.residence_report_prospective_tenant, tenant);
+        }
+        String report = "Location " + geolocation + " Date: " + dateString() + " " + rentedString + " " + prospectiveTenant;
+        return report;
+
     }
 
 }
