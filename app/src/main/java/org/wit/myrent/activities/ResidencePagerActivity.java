adba3c2caee1953d34b4d6d797f4140fbf953f49
austincunningham <austincunningham@oceanfree.net>
2016-10-10 13:20:59 +0100

    Create ResidencePagerActivity

---
+++ app/src/main/java/org/wit/myrent/activities/ResidencePagerActivity.java
@@ -0,0 +1,48 @@
+package org.wit.myrent.activities;
+
+/**
+ * Created by ictskills on 10/10/16.
+ */
+ import android.os.Bundle;
+ import android.support.v4.view.PagerAdapter;
+ import android.support.v4.view.ViewPager;
+ import android.support.v7.app.AppCompatActivity;
+ import android.support.v4.view.ViewPager;
+
+ import org.wit.myrent.R;
+ import org.wit.myrent.app.MyRentApp;
+ import org.wit.myrent.models.Portfolio;
+ import org.wit.myrent.models.Residence;
+
+ import java.util.ArrayList;
+
+public class ResidencePagerActivity extends AppCompatActivity
+{
+    private ViewPager viewPager;
+    private ArrayList<Residence> residences;
+    private Portfolio portfolio;
+    private PagerAdapter pagerAdapter;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        viewPager = new ViewPager(this);
+        viewPager.setId(R.id.viewPager);
+        setContentView(viewPager);
+
+        setResidenceList();
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager(), residences);
+        viewPager.setAdapter(pagerAdapter);
+
+    }
+
+    private void setResidenceList()
+    {
+        MyRentApp app = (MyRentApp) getApplication();
+        portfolio = app.portfolio;
+        residences = portfolio.residences;
+    }
+}
