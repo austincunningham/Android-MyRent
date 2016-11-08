c1869ce8af25c03809b91b8064815994c1c7dfbf
austincunningham <austincunningham@hotmail.com>
2016-11-08 21:43:26 +0000

    info window added to marker,long press moves marker,zoom controls and action bar.

--- app/src/main/java/org/wit/myrent/activities/MapBoxActivity.java
+++ app/src/main/java/org/wit/myrent/activities/MapBoxActivity.java
@@ -1,6 +1,5 @@
 package org.wit.myrent.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -23,38 +22,88 @@ import org.wit.myrent.models.Residence;
 
 import static org.wit.android.helpers.IntentHelper.navigateUp;
 
-
-public class MapBoxActivity extends Activity implements OnMapReadyCallback{
+public class MapBoxActivity extends AppCompatActivity implements
+        OnMapReadyCallback,
+        MapboxMap.OnMarkerClickListener,
+        MapboxMap.OnMapLongClickListener
+{
 
     private MapView mapView;
     private MapboxMap mapboxMap;
+    private Marker residenceMarker;
+    /*
+  * We use the current residence when navigating back to parent class - ResidenceFragment as
+  * this is required in ResidenceFragment onCreate. The navigateUp
+  */
     Long resId; // The id of the residence associate with this map pane
     Residence residence; // The residence associated with this map pane
     LatLng residenceLatLng;
     MyRentApp app;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Mapbox access token only needs to be configured once in your app
-        MapboxAccountManager.start(this, "pk.eyJ1IjoiYXVzdGluY3VubmluZ2hhbSIsImEiOiJjaXY5bWZ1MTkwMDExMnpwZnhjeGphM3QxIn0.z33AOo3eeJHgAdcmRvPDGw");
-
-        // This contains the MapView in XML and needs to be called after the account manager
-        setContentView(R.layout.activity_mapbox);
-
-
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
         resId = (Long) getIntent().getSerializableExtra(ResidenceFragment.EXTRA_RESIDENCE_ID);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         app = (MyRentApp) getApplication();
         residence = app.portfolio.getResidence(resId);
         if (residence != null) {
             residenceLatLng = new LatLng(MapHelper.latitude(residence.geolocation),
                     MapHelper.longitude(residence.geolocation));
         }
+        // Mapbox access token only needs to be configured once in your app
+        MapboxAccountManager.start(this, "pk.eyJ1IjoidXNwbGl0dSIsImEiOiI3Mm90ZXR3In0.-XsJBar8tOXqlLjyrXX01Q");
+
+        // This contains the MapView in XML and needs to be called after the account manager
+        setContentView(R.layout.activity_mapbox);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                navigateUp(this);
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // OnMapReadyCallback interface method impl
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        positionCamera();
+        setMarker();
+        mapboxMap.getUiSettings().setZoomControlsEnabled(true);
+        mapboxMap.getUiSettings().setZoomGesturesEnabled(true);
+        mapboxMap.setOnMarkerClickListener(this);
+        mapboxMap.setOnMapLongClickListener(this);
+
+    }
+
+    private void setMarker() {
+
+        MarkerViewOptions marker = new MarkerViewOptions().position(residenceLatLng);
+        residenceMarker = mapboxMap.addMarker(marker);
+    }
+
+    private void positionCamera() {
+        CameraPosition position = new CameraPosition.Builder()
+                .target(residenceLatLng) // Sets the new camera position
+                .zoom(residence.zoom)
+                .build(); // Creates a CameraPosition from the builder
+
+        mapboxMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(position));
+
     }
 
     // Add the mapView lifecycle to the activity's lifecycle methods
@@ -68,6 +117,8 @@ public class MapBoxActivity extends Activity implements OnMapReadyCallback{
     public void onPause() {
         super.onPause();
         mapView.onPause();
+        residence.zoom = mapboxMap.getCameraPosition().zoom;
+        residence.geolocation = MapHelper.latLng(residenceMarker.getPosition());
     }
 
     @Override
@@ -88,27 +139,26 @@ public class MapBoxActivity extends Activity implements OnMapReadyCallback{
         mapView.onSaveInstanceState(outState);
     }
 
-    // OnMapReadyCallback interface method impl
+    // OnMarkerClickListener
     @Override
-    public void onMapReady(MapboxMap mapboxMap) {
-        this.mapboxMap = mapboxMap;
-        positionCamera();
-        setMarker();
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        String snippet = "GPS : " + residence.geolocation;
+        marker.setSnippet(snippet);
+        return false;
     }
 
-    private void setMarker() {
 
-        MarkerViewOptions marker = new MarkerViewOptions().position(residenceLatLng);
-        mapboxMap.addMarker(marker);
+    /**
+     * Long click moves marker to clicked position and updates
+     * Residence object's geolocation to new marker position.
+     * @param point
+     */
+    // OnMapLonClickListener
+    @Override
+    public void onMapLongClick(@NonNull LatLng point) {
+        residenceMarker.setPosition(point);
     }
 
-    private void positionCamera() {
-        CameraPosition position = new CameraPosition.Builder()
-                .target(residenceLatLng) // Sets the new camera position
-                .zoom(residence.zoom) // Sets the zoom
-                .build(); // Creates a CameraPosition from the builder
-
-        mapboxMap.animateCamera(CameraUpdateFactory
-                .newCameraPosition(position));
-    }
 }
+
+
