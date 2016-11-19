e5b6977f8b46cd521b916cce678435db890ca0d3
austincunningham <austincunningham@hotmail.com>
2016-11-19 08:13:52 +0000

    added Gson builder and retrofit to MyRentApp

--- app/src/main/java/org/wit/myrent/app/MyRentApp.java
+++ app/src/main/java/org/wit/myrent/app/MyRentApp.java
@@ -4,11 +4,22 @@ import android.app.Application;
 import android.util.Log;
 import org.wit.myrent.models.Portfolio;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import org.wit.myrent.retrofit.ResidenceServiceProxy;
+import java.util.concurrent.TimeUnit;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
 
 public class MyRentApp extends Application
 {
     static final String TAG = "MyRentApp";
     public Portfolio portfolio;
+    public String service_url = "http://10.0.2.2:9000"; //Android Emulator
+    //public String service_url = "https://myrent-service-2016.herokuapp.com/";
+    public ResidenceServiceProxy residenceService;
 
     protected static MyRentApp app;
     @Override
@@ -18,6 +29,14 @@ public class MyRentApp extends Application
         portfolio = new Portfolio(getApplicationContext());
         Log.d(TAG, "MyRent app launched");
         app = this;
+
+        Gson gson = new GsonBuilder().create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(service_url)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        residenceService = retrofit.create(ResidenceServiceProxy.class);
     }
 
     public static MyRentApp getApp(){
